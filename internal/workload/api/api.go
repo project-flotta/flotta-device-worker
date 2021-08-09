dac94d9377aadb73276c4e8cf4328f8b81686c26
Jakub Dzon <jdzon@redhat.com>
2021-08-09 15:25:20 +0200

    Removed unused interface
    Signed-off-by: Jakub Dzon <jdzon@redhat.com>

--- internal/workload/api/api.go
+++ internal/workload/api/api.go
@@ -1,15 +1,6 @@
 package api
 
-import v1 "k8s.io/api/core/v1"
-
 type WorkloadInfo struct {
 	Name   string
 	Status string
-}
-
-type WorkloadAPI interface {
-	List() ([]WorkloadInfo, error)
-	Remove(workloadName string) error
-	Run(workload *v1.Pod, manifestPath string) error
-	Start(workload *v1.Pod) error
 }
